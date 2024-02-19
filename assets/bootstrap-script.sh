cdb41511b7ff5e581a41fe3e5c95ccbba1db3d38
Anders Ingemann <aim@orbit.online>
2024-02-19 21:29:01 +0100

    Initial import: FAI config for TrueNAS

---
+++ assets/bootstrap-script.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Source: https://github.com/andsens/bootstrap-vz/blob/fcdc6993f59e521567fb101302b02312e741b88c/bootstrapvz/plugins/minimize_size/assets/bootstrap-script.sh
+
+extract_dpkg_deb_data () {
+  pkg="$1"
+  exclude_files=$(mktemp)
+  dpkg-deb --fsys-tarfile "$pkg" | tar -t | \
+      grep '\./usr/share/locale/.\+\|\./usr/share/man/.\+\|\./usr/share/doc/.\+' | \
+      grep --invert-match --fixed-strings '/usr/share/locale/locale.alias
+/usr/share/man/man1
+/usr/share/man/man2
+/usr/share/man/man3
+/usr/share/man/man4
+/usr/share/man/man5
+/usr/share/man/man6
+/usr/share/man/man7
+/usr/share/man/man8
+/usr/share/man/man9
+/var/lib/apt/lists
+/var/cache/apt' >"$exclude_files" || true
+  # List all files in $pkg and run them through the filter
+  dpkg-deb --fsys-tarfile "$pkg" | tar --exclude-from "$exclude_files" -xf -
+  rm "$exclude_files"
+}
+
+# Direct copypasta from the debootstrap script where it determines
+# which script to run. We do exactly the same but leave out the
+# if [ "$4" != "" ] part so that we can source the script that
+# should've been sourced in this scripts place.
+
+SCRIPT="$DEBOOTSTRAP_DIR/scripts/$SUITE"
+if [ -n "$VARIANT" ] && [ -e "${SCRIPT}.${VARIANT}" ]; then
+  SCRIPT="${SCRIPT}.${VARIANT}"
+fi
+
+# shellcheck disable=SC1090
+. "$SCRIPT"
---
+++ bootstrap.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+set -eo pipefail; shopt -s inherit_errexit
+PKGROOT=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+PATH=$("$PKGROOT/.upkg/.bin/path_prepend" "$PKGROOT/.upkg/.bin")
+
+main() {
+  source "$PKGROOT/.upkg/orbit-online/records.sh/records.sh"
+  source "$PKGROOT/.upkg/orbit-online/collections.sh/collections.sh"
+
+  DOC="bootstrap.sh - Bootstrap k3s cluster images
+Usage:
+  bootstrap create HOSTNAME
+  bootstrap mount HOSTNAME
+  bootstrap boot HOSTNAME
+"
+# docopt parser below, refresh this parser with `docopt.sh bootstrap.sh`
+# shellcheck disable=2016,1090,1091,2034
+docopt() { source "$PKGROOT/.upkg/andsens/docopt.sh/docopt-lib.sh" '1.0.0' || {
+ret=$?; printf -- "exit %d\n" "$ret"; exit "$ret"; }; set -e
+trimmed_doc=${DOC:0:131}; usage=${DOC:44:87}; digest=8a844; shorts=(); longs=()
+argcounts=(); node_0(){ value HOSTNAME a; }; node_1(){ _command create; }
+node_2(){ _command mount; }; node_3(){ _command boot; }; node_4(){ required 1 0
+}; node_5(){ required 2 0; }; node_6(){ required 3 0; }; node_7(){ either 4 5 6
+}; node_8(){ required 7; }; cat <<<' docopt_exit() {
+[[ -n $1 ]] && printf "%s\n" "$1" >&2; printf "%s\n" "${DOC:44:87}" >&2; exit 1
+}'; unset var_HOSTNAME var_create var_mount var_boot; parse 8 "$@"
+local prefix=${DOCOPT_PREFIX:-''}; unset "${prefix}HOSTNAME" "${prefix}create" \
+"${prefix}mount" "${prefix}boot"; eval "${prefix}"'HOSTNAME=${var_HOSTNAME:-}'
+eval "${prefix}"'create=${var_create:-false}'
+eval "${prefix}"'mount=${var_mount:-false}'
+eval "${prefix}"'boot=${var_boot:-false}'; local docopt_i=1
+[[ $BASH_VERSION =~ ^4.3 ]] && docopt_i=2; for ((;docopt_i>0;docopt_i--)); do
+declare -p "${prefix}HOSTNAME" "${prefix}create" "${prefix}mount" \
+"${prefix}boot"; done; }
+# docopt parser above, complete command for generating this parser is `docopt.sh --library='"$PKGROOT/.upkg/andsens/docopt.sh/docopt-lib.sh"' bootstrap.sh`
+  eval "$(docopt "$@")"
+
+  if [[ $UID != 0 ]]; then
+    fatal "Run with sudo"
+  fi
+  : "${SUDO_UID:?"\$SUDO_UID is not set, run with sudo"}"
+
+  sudo -u "#$SUDO_UID" mkdir -p "$PKGROOT/images"
+  # shellcheck disable=SC2154
+  if $create; then
+    create_image "$HOSTNAME"
+  elif $mount; then
+    mount_image "$HOSTNAME"
+  elif $boot; then
+    boot_image "$HOSTNAME"
+  fi
+}
+
+get_image_name() {
+  local hostname=$1
+  printf "%s/images/%s.raw" "$PKGROOT" "$hostname"
+}
+
+create_image() {
+  local hostname=$1 image
+  image=$(get_image_name "$hostname")
+  env - \
+    "PATH=$PATH" \
+    "PKGROOT=$PKGROOT" \
+    fai-diskimage --verbose --cspace "$PKGROOT/config" --new --size 2G --hostname "$hostname" "$image"
+  sudo chown "$SUDO_UID:$SUDO_UID" "$image"
+}
+
+mount_image() {
+  local hostname=$1 mountpath image devpath
+  mountpath=$PKGROOT/mount/$hostname
+  mkdir -p "$mountpath"
+  image=$(get_image_name "$hostname")
+  devpath=$(losetup --show --find --partscan images/k8s-nas.raw)
+  # shellcheck disable=SC2064
+  trap "umount \"$mountpath/boot/efi\"; umount \"$mountpath\"; losetup --detach \"$devpath\"; exit 1" EXIT SIGINT SIGTERM
+  mount "${devpath}p2" "$mountpath"
+  mount "${devpath}p1" "$mountpath/boot/efi"
+  info "image %s mounted at %s, press <ENTER> to unmount" "${image#"$PKGROOT/"}" "${mountpath#"$PKGROOT/"}"
+  local _read
+  read -r _read
+  umount "$mountpath/boot/efi" "$mountpath"
+  losetup --detach "$devpath"
+  trap "" EXIT
+}
+
+boot_image() {
+  local hostname=$1 image
+  image=$(get_image_name "$hostname")
+  kvm -bios /usr/share/ovmf/OVMF.fd \
+    -k en-us -smp 2 -cpu host -m 2000 -name "$hostname" \
+    -boot order=c -device virtio-net-pci,netdev=net0 -netdev user,id=net0 \
+    -drive "file=$image,if=none,format=raw,id=nvme1" -device nvme,serial=SN123450001,drive=nvme1
+}
+
+main "$@"
