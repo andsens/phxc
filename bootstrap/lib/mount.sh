732a8cf70650bb455619abd2d2564e49d5b3b260
Anders Ingemann <aim@orbit.online>
2024-03-14 18:04:30 +0100

    k8s-nas: Let root be rw, and then mount second disk for k3s data

--- bootstrap/lib/mount.sh
+++ bootstrap/lib/mount.sh
@@ -2,27 +2,32 @@
 # shellcheck disable=2064,2030,2031
 
 mount_image() {
-  local image_path=$1 devpath
-  if [[ -z $2 ]]; then
+  local image_path=$1 disk=$2 dev_path
+  if [[ -z $3 ]]; then
     MOUNT_PATH=$(mktemp -d)
   else
-    local MOUNT_PATH=$2
+    local MOUNT_PATH=$3
   fi
-  devpath=$(losetup --show --find --partscan "$image_path")
+  dev_path=$(losetup --show --find --partscan "$image_path")
   if [[ -z $2 ]]; then
-    trap "umount_image \"$MOUNT_PATH\" \"$devpath\" true" EXIT SIGINT SIGTERM
+    trap "umount_image \"$MOUNT_PATH\" \"$dev_path\" \"$disk\" true" EXIT SIGINT SIGTERM
   else
-    trap "umount_image \"$MOUNT_PATH\" \"$devpath\" false" EXIT SIGINT SIGTERM
+    trap "umount_image \"$MOUNT_PATH\" \"$dev_path\" \"$disk\" false" EXIT SIGINT SIGTERM
   fi
-  mount "${devpath}p2" "$MOUNT_PATH"
-  mount "${devpath}p1" "$MOUNT_PATH/boot/efi"
+  case "$disk" in
+    root) mount "${dev_path}p2" "$MOUNT_PATH"; mount "${dev_path}p1" "$MOUNT_PATH/boot/efi" ;;
+    *) mount "$dev_path" "$MOUNT_PATH" ;;
+  esac
 }
 
 umount_image() {
-  local mount_path=$1 devpath=$2 rm_mountpath=${3:-false} timeout ret=0
-  if ! umount -l "$mount_path/boot/efi" "$mount_path"; then
+  local mount_path=$1 dev_path=$2 disk=$3 rm_mountpath=$4 timeout ret=0
+  case "$disk" in
+    root) umount -l "$mount_path/boot/efi" "$mount_path" || ret=$? ;;
+    *) umount -l "$mount_path" || ret=$? ;;
+  esac
+  if [[ $ret != 0 ]] ; then
     warning "Failed to unmount %s" "$mount_path"
-    ret=1
   else
     timeout=1000
     while mount | grep -q "$mount_path"; do
@@ -30,8 +35,8 @@ umount_image() {
       [[ $timeout -gt 0 ]] || { warning "Timed out waiting for %s to unmount" "$mount_path"; break; }
     done
   fi
-  if ! losetup --detach "$devpath"; then
-    warning "Failed to detach loopdevice %s" "$devpath"
+  if ! losetup --detach "$dev_path"; then
+    warning "Failed to detach loopdevice %s" "$dev_path"
     ret=1
   fi
   if $rm_mountpath; then
