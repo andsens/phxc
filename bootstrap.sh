e0982c5502608ffd0defdf03e2e750e635624e29
Anders Ingemann <aim@orbit.online>
2024-03-31 13:11:40 +0200

    Get bootstrapper image working

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck source-path=.. disable=2064
+# shellcheck source-path=..
 
 set -eo pipefail; shopt -s inherit_errexit
 PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")
@@ -11,66 +11,83 @@ main() {
 
   DOC="bootstrap.sh - Bootstrap k3s cluster images
 Usage:
-  bootstrap create HOSTNAME [IMAGESIZE]
-  bootstrap mount IMAGEPATH
-  bootstrap boot IMAGEPATH
+  bootstrap create [options] HOSTNAME [IMGPATH]
+  bootstrap mount IMGPATH
+  bootstrap boot IMGPATH
+
+Options:
+  --imgsize=PATH    Size of the root disk [default: 1.5G]
+  --varspath=PATH   Path to vars.sh [default: \$PKGROOT/vars.sh]
+  --cachepath=PATH  Path to the cache dir [default: \$PKGROOT/bootstrap/cache]
 "
 # docopt parser below, refresh this parser with `docopt.sh bootstrap.sh`
 # shellcheck disable=2016,1090,1091,2034
 docopt() { source "$PKGROOT/.upkg/andsens/docopt.sh/docopt-lib.sh" '1.0.0' || {
 ret=$?; printf -- "exit %d\n" "$ret"; exit "$ret"; }; set -e
-trimmed_doc=${DOC:0:145}; usage=${DOC:44:101}; digest=b5f27; shorts=(); longs=()
-argcounts=(); node_0(){ value HOSTNAME a; }; node_1(){ value IMAGESIZE a; }
-node_2(){ value IMAGEPATH a; }; node_3(){ _command create; }; node_4(){
-_command mount; }; node_5(){ _command boot; }; node_6(){ optional 1; }
-node_7(){ required 3 0 6; }; node_8(){ required 4 2; }; node_9(){ required 5 2
-}; node_10(){ either 7 8 9; }; node_11(){ required 10; }
-cat <<<' docopt_exit() { [[ -n $1 ]] && printf "%s\n" "$1" >&2
-printf "%s\n" "${DOC:44:101}" >&2; exit 1; }'; unset var_HOSTNAME \
-var_IMAGESIZE var_IMAGEPATH var_create var_mount var_boot; parse 11 "$@"
-local prefix=${DOCOPT_PREFIX:-''}; unset "${prefix}HOSTNAME" \
-"${prefix}IMAGESIZE" "${prefix}IMAGEPATH" "${prefix}create" "${prefix}mount" \
-"${prefix}boot"; eval "${prefix}"'HOSTNAME=${var_HOSTNAME:-}'
-eval "${prefix}"'IMAGESIZE=${var_IMAGESIZE:-}'
-eval "${prefix}"'IMAGEPATH=${var_IMAGEPATH:-}'
+trimmed_doc=${DOC:0:359}; usage=${DOC:44:105}; digest=dd18e; shorts=('' '' '')
+longs=(--varspath --cachepath --imgsize); argcounts=(1 1 1); node_0(){
+value __varspath 0; }; node_1(){ value __cachepath 1; }; node_2(){
+value __imgsize 2; }; node_3(){ value HOSTNAME a; }; node_4(){ value IMGPATH a
+}; node_5(){ _command create; }; node_6(){ _command mount; }; node_7(){
+_command boot; }; node_8(){ optional 0 1 2; }; node_9(){ optional 8; }
+node_10(){ optional 4; }; node_11(){ required 5 9 3 10; }; node_12(){
+required 6 4; }; node_13(){ required 7 4; }; node_14(){ either 11 12 13; }
+node_15(){ required 14; }; cat <<<' docopt_exit() {
+[[ -n $1 ]] && printf "%s\n" "$1" >&2; printf "%s\n" "${DOC:44:105}" >&2; exit 1
+}'; unset var___varspath var___cachepath var___imgsize var_HOSTNAME \
+var_IMGPATH var_create var_mount var_boot; parse 15 "$@"
+local prefix=${DOCOPT_PREFIX:-''}; unset "${prefix}__varspath" \
+"${prefix}__cachepath" "${prefix}__imgsize" "${prefix}HOSTNAME" \
+"${prefix}IMGPATH" "${prefix}create" "${prefix}mount" "${prefix}boot"
+eval "${prefix}"'__varspath=${var___varspath:-'"'"'$PKGROOT/vars.sh'"'"'}'
+eval "${prefix}"'__cachepath=${var___cachepath:-'"'"'$PKGROOT/bootstrap/cache'"'"'}'
+eval "${prefix}"'__imgsize=${var___imgsize:-1.5G}'
+eval "${prefix}"'HOSTNAME=${var_HOSTNAME:-}'
+eval "${prefix}"'IMGPATH=${var_IMGPATH:-}'
 eval "${prefix}"'create=${var_create:-false}'
 eval "${prefix}"'mount=${var_mount:-false}'
 eval "${prefix}"'boot=${var_boot:-false}'; local docopt_i=1
 [[ $BASH_VERSION =~ ^4.3 ]] && docopt_i=2; for ((;docopt_i>0;docopt_i--)); do
-declare -p "${prefix}HOSTNAME" "${prefix}IMAGESIZE" "${prefix}IMAGEPATH" \
-"${prefix}create" "${prefix}mount" "${prefix}boot"; done; }
+declare -p "${prefix}__varspath" "${prefix}__cachepath" "${prefix}__imgsize" \
+"${prefix}HOSTNAME" "${prefix}IMGPATH" "${prefix}create" "${prefix}mount" \
+"${prefix}boot"; done; }
 # docopt parser above, complete command for generating this parser is `docopt.sh --library='"$PKGROOT/.upkg/andsens/docopt.sh/docopt-lib.sh"' bootstrap.sh`
   eval "$(docopt "$@")"
 
   # shellcheck disable=SC2154
   if $create; then
-    local run_env=env image_path="$PKGROOT/bootstrap/images/$HOSTNAME.raw"
+    local run_env=env
     [[ $UID = 0 ]] || run_env="sudo env"
-    mkdir -p "$PKGROOT/bootstrap/images" "$PKGROOT/bootstrap/logs"
+    IMGPATH=${IMGPATH:-"$PKGROOT/bootstrap/images/$HOSTNAME.raw"}
+    [[ $__varspath != "\$PKGROOT/vars.sh" ]] || __varspath=$PKGROOT/vars.sh
+    [[ $__cachepath != "\$PKGROOT/bootstrap/cache" ]] || __cachepath=$PKGROOT/bootstrap/cache
+    mkdir -p "$(dirname "$IMGPATH")" "$PKGROOT/bootstrap/logs"
     rm -f "$PKGROOT/bootstrap/logs/$HOSTNAME"
     ln -s "/var/log/fai/$HOSTNAME/last" "$PKGROOT/bootstrap/logs/$HOSTNAME"
     # shellcheck disable=SC2086
     $run_env - \
       "PATH=$PATH" \
       "PKGROOT=$PKGROOT" \
-      fai-diskimage --cspace "$PKGROOT/bootstrap/config" --new --size "${IMAGESIZE:-1.5G}" --hostname "$HOSTNAME" "$image_path"
+      "VARSPATH=$__varspath" \
+      "CACHEPATH=$__cachepath" \
+      fai-diskimage --cspace "$PKGROOT/bootstrap/config" --new --size "${__imgsize:-1.5G}" --hostname "$HOSTNAME" "$IMGPATH"
     if [[ $UID != 0 ]]; then
-      sudo chown "$UID:$UID" "$image_path"
-      sudo chown -R "$UID:$UID" "$PKGROOT/bootstrap/cache" "$image_path"
+      sudo chown "$UID:$UID" "$IMGPATH"
+      sudo chown -R "$UID:$UID" "$__cachepath" "$IMGPATH"
     fi
   elif $mount; then
     local mount_path
-    mount_path=$PKGROOT/bootstrap/mnt/$(basename "$IMAGEPATH" .raw)
+    mount_path=$PKGROOT/bootstrap/mnt/$(basename "$IMGPATH" .raw)
     mkdir -p "$mount_path"
-    mount_image "$IMAGEPATH" "$mount_path"
-    info "image %s mounted at %s, press <ENTER> to unmount" "${IMAGEPATH#"$PKGROOT/"}" "${mount_path#"$PKGROOT/"}"
+    mount_image "$IMGPATH" "$mount_path"
+    info "image %s mounted at %s, press <ENTER> to unmount" "${IMGPATH#"$PKGROOT/"}" "${mount_path#"$PKGROOT/"}"
     local _read
     read -rs _read
   elif $boot; then
     kvm -bios /usr/share/ovmf/OVMF.fd \
-      -k en-us -smp 2 -cpu host -m 2000 -name "$(basename "$IMAGEPATH" .raw)" \
+      -k en-us -smp 2 -cpu host -m 2000 -name "$(basename "$IMGPATH" .raw)" \
       -boot order=c -device virtio-net-pci,netdev=net0 -netdev user,id=net0 \
-      -drive "file=$IMAGEPATH,if=none,format=raw,id=nvme1" -device nvme,serial=SN123450001,drive=nvme1
+      -drive "file=$IMGPATH,if=none,format=raw,id=nvme1" -device nvme,serial=SN123450001,drive=nvme1
   fi
 }
 
--- vars.sh.tpl
+++ vars.sh.tpl
@@ -35,15 +35,23 @@ ADMIN_NFS_HOME_SHARE=/mnt/cluster/home/...
 #############################
 
 MACHINE_IDS['bootstrapper']='b8b235b3279c6054bc9f33ef6609287a'
-TRUENAS_HOST_BRIDGE_CLIENT_IPs[bootstrapper]="10.15.180.3/24,fd25:9998:d0e7:e351:1a01:be9:4d9a:157f/48"
-BOOTSTRAPPER_NFS_IMAGES_SHARE=/mnt/cluster/images
+TRUENAS_HOST_BRIDGE_CLIENT_IPs['bootstrapper']="10.15.180.3/24,fd25:9998:d0e7:e351:1a01:be9:4d9a:157f/48"
+BOOTSTRAPPER_NFS_SHARE=/mnt/cluster/bootstrapper
+BOOTSTRAPPER_GIT_REMOTE=https://github.com/andsens/home-cluster.git
+# Leave empty if using a public repo
+BOOTSTRAPPER_GIT_DEPLOY_KEY=
+BOOTSTRAPPER_GIT_REMOTE_SSH_KEYS=
+# When cloning via ssh you will need to trust the remote. For GitHub the keys are:
+# BOOTSTRAPPER_GIT_REMOTE_SSH_KEYS='|1|8jHpq1TRdjyRbwBRcH23I2OLtF8=|uk+0m7ScMP+V/M4ZfiVkyohWKDA= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+# |1|uQiPao7vfX6EtebIPrakQdTFeGA=|9xWf68bM3DiK+EkkE4T/ExnNHbg= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+# |1|E/XNHmuoBRKRPJu+eHMyjYli4g0=|q735yv4k6923UkPMM9b78WgBFTA= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl'
 
 #########################
 ### Cluster settings ###
 #########################
 
 MACHINE_IDS['k8s-nas']='2a178ed534ac2a67dbc8049d65eddc45'
-TRUENAS_HOST_BRIDGE_CLIENT_IPs[k8s-nas]="10.15.180.2/24,fd25:9998:d0e7:e351:1a01:be9:4d9a:157e/48"
+TRUENAS_HOST_BRIDGE_CLIENT_IPs['k8s-nas']="10.15.180.2/24,fd25:9998:d0e7:e351:1a01:be9:4d9a:157e/48"
 
 # Name of the cluster, used in various contexts to identify it outwardly
 # Spaces are allowed and might be replaced with _ or - depending on the context
