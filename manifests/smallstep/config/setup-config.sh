7a9279931318f70b24649b480c959dc16ac0af63
Anders Ingemann <aim@orbit.online>
2024-04-25 22:06:31 +0200

    smallstep: Set the external domain name

--- manifests/smallstep/config/setup-config.sh
+++ manifests/smallstep/config/setup-config.sh
@@ -2,8 +2,9 @@
 set -Eeo pipefail
 
 main() {
-  : "${STEPPATH:?}"
-  cp "$STEPPATH/config-ro/ca.json" "$STEPPATH/config/ca.json"
+  : "${STEPPATH:?}" "${STEP_CA_DOMAIN:?}"
+  apk add --update --no-cache jq
+  jq --arg domain "$STEP_CA_DOMAIN" '.dnsNames+=[$domain]' "$STEPPATH/config-ro/ca.json" > "$STEPPATH/config/ca.json"
   printf "setup-config.sh: Adding step-issuer to ca.json\n" >&2
   step ca provisioner add step-issuer --type JWK \
     --public-key="$STEPPATH/certs/issuer-provisioner/pub.json" \
