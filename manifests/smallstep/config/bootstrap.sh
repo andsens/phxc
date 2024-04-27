5ca9d09d73599b4cadcd4ab9f815b64454e342bc
Anders Ingemann <aim@orbit.online>
2024-04-27 18:58:51 +0200

    Remove ssh-jwt job and store both root & intermediate key/cert persistently

--- manifests/smallstep/config/bootstrap.sh
+++ manifests/smallstep/config/bootstrap.sh
@@ -12,14 +12,14 @@ setup_certificates() {
   printf "bootstrap.sh: Setting up root and intermediate certificates\n" >&2
 
   local root_ca_is_new=false
-  if [[ ! -e "$STEPPATH/certs/root/tls.key" ]]; then
-    rm -f "$STEPPATH/certs/root/tls.crt"
+  if [[ ! -e "$STEPPATH/persistent-certs/root_ca.key" ]]; then
+    rm -f "$STEPPATH/persistent-certs/root_ca.crt"
     printf "bootstrap.sh: Root certificate does not exist on PV, creating now\n" >&2
     root_ca_is_new=true
     step certificate create --profile=root-ca \
       --no-password --insecure \
       --not-after=87600h \
-      "$CLUSTER_NAME Root" "$STEPPATH/certs/root/tls.crt" "$STEPPATH/certs/root/tls.key"
+      "$CLUSTER_NAME Root" "$STEPPATH/persistent-certs/root_ca.crt" "$STEPPATH/persistent-certs/root_ca.key"
   else
     printf "bootstrap.sh: Root certificate exists on PV, skipping creation\n" >&2
   fi
@@ -27,29 +27,28 @@ setup_certificates() {
   if $root_ca_is_new || ! kubectl get -n "$NAMESPACE" secret smallstep-root -o jsonpath='{.data.tls\.crt}' >/dev/null; then
     printf "bootstrap.sh: Root certificate secret does not exist or has been recreated, creating now\n" >&2
     kubectl create -n "$NAMESPACE" secret tls smallstep-root \
-      --cert="$STEPPATH/certs/root/tls.crt" \
-      --key="$STEPPATH/certs/root/tls.key" || \
+      --cert="$STEPPATH/persistent-certs/root_ca.crt" \
+      --key="$STEPPATH/persistent-certs/root_ca.key" || \
     kubectl replace -n "$NAMESPACE" secret tls smallstep-root \
-      --cert="$STEPPATH/certs/root/tls.crt" \
-      --key="$STEPPATH/certs/root/tls.key"
+      --cert="$STEPPATH/persistent-certs/root_ca.crt" \
+      --key="$STEPPATH/persistent-certs/root_ca.key"
   else
     printf "bootstrap.sh: Root certificate secret exists, skipping creation\n" >&2
   fi
 
   if $root_ca_is_new || ! kubectl get -n "$NAMESPACE" secret smallstep-intermediate >/dev/null; then
     printf "bootstrap.sh: Intermediate certificate does not exist or the root has been recreated, creating now\n" >&2
-    mkdir "$STEPPATH/certs/intermediate"
     step certificate create --profile=intermediate-ca \
       --no-password --insecure \
       --not-after=87600h \
-      --ca="$STEPPATH/certs/root/tls.crt" --ca-key="$STEPPATH/certs/root/tls.key" \
-      "$CLUSTER_NAME Intermediate" "$STEPPATH/certs/intermediate/tls.crt" "$STEPPATH/certs/intermediate/tls.key"
+      --ca="$STEPPATH/persistent-certs/root_ca.crt" --ca-key="$STEPPATH/persistent-certs/root_ca.key" \
+      "$CLUSTER_NAME Intermediate" "$STEPPATH/persistent-certs/intermediate_ca.crt" "$STEPPATH/persistent-certs/intermediate_ca.key"
     kubectl create -n "$NAMESPACE" secret tls smallstep-intermediate \
-      --cert="$STEPPATH/certs/intermediate/tls.crt" \
-      --key="$STEPPATH/certs/intermediate/tls.key" || \
+      --cert="$STEPPATH/persistent-certs/intermediate_ca.crt" \
+      --key="$STEPPATH/persistent-certs/intermediate_ca.key" || \
     kubectl replace -n "$NAMESPACE" secret tls smallstep-intermediate \
-      --cert="$STEPPATH/certs/intermediate/tls.crt" \
-      --key="$STEPPATH/certs/intermediate/tls.key"
+      --cert="$STEPPATH/persistent-certs/intermediate_ca.crt" \
+      --key="$STEPPATH/persistent-certs/intermediate_ca.key"
   else
     printf "bootstrap.sh: Intermediate certificate exists, skipping creation\n" >&2
   fi
@@ -75,7 +74,7 @@ setup_issuer_provisioner() {
   fi
 
   kubectl get stepclusterissuer step-issuer -ojsonpath='{.spec.caBundle}' | base64 -d >"$STEPPATH/certs/step-issuer-provisioner/caBundle.key" || true
-  if $jwk_is_new || ! diff -q "$STEPPATH/certs/root/tls.crt" "$STEPPATH/certs/step-issuer-provisioner/caBundle.key"; then
+  if $jwk_is_new || ! diff -q "$STEPPATH/persistent-certs/root_ca.crt" "$STEPPATH/certs/step-issuer-provisioner/caBundle.key"; then
     printf "bootstrap.sh: StepClusterIssuer does not exist, the provisioner JWK has been recreated, or the caBundle is incorrect, creating now\n" >&2
     kubectl apply -f <(printf -- "apiVersion: certmanager.step.sm/v1beta1
 kind: StepClusterIssuer
@@ -90,7 +89,7 @@ spec:
     passwordRef:
       namespace: smallstep
       name: step-issuer-provisioner-password
-      key: password" "$(base64 -w0 "$STEPPATH/certs/root/tls.crt")" "$(step crypto jwk thumbprint < "$STEPPATH/certs/step-issuer-provisioner/pub.json")"
+      key: password" "$(base64 -w0 "$STEPPATH/persistent-certs/root_ca.crt")" "$(step crypto jwk thumbprint < "$STEPPATH/certs/step-issuer-provisioner/pub.json")"
     )
   else
     printf "bootstrap.sh: StepClusterIssuer exists, skipping creation\n" >&2
@@ -102,14 +101,14 @@ setup_ssh_host_provisioner() {
   local jwk_is_new=false
 
   mkdir "$STEPPATH/certs/ssh-host-provisioner"
-  if ! kubectl get -n "$NAMESPACE" secret step-ssh-host-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$STEPPATH/certs/ssh-host-provisioner/pub.json"; then
+  if ! kubectl get -n "$NAMESPACE" secret ssh-host-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$STEPPATH/certs/ssh-host-provisioner/pub.json"; then
     printf "bootstrap.sh: ssh-host-provisioner provisioner JWK does not exist, creating now\n" >&2
     rm -f "$STEPPATH/certs/ssh-host-provisioner/pub.json"
     step crypto jwk create \
       --password-file="$STEPPATH/ssh-host-provisioner-password/password" \
       --use sig \
       "$STEPPATH/certs/ssh-host-provisioner/pub.json" "$STEPPATH/certs/ssh-host-provisioner/priv.json"
-    kubectl create -n "$NAMESPACE" secret generic step-ssh-host-provisioner \
+    kubectl create -n "$NAMESPACE" secret generic ssh-host-provisioner \
       --from-file="$STEPPATH/certs/ssh-host-provisioner/pub.json" \
       --from-file="$STEPPATH/certs/ssh-host-provisioner/priv.json"
   else
