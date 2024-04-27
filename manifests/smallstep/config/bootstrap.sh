273dc257e480322254787a3ba424961aa88fa283
Anders Ingemann <aim@orbit.online>
2024-04-27 22:08:18 +0200

    smallstep: Allow better provisioner config by merging existing config with added keys config

--- manifests/smallstep/config/bootstrap.sh
+++ manifests/smallstep/config/bootstrap.sh
@@ -1,56 +1,63 @@
 #!/bin/bash
 set -Eeo pipefail
 
+: "${CLUSTER_NAME:?}" "${STEPPATH:?}" "${NAMESPACE:?}"
+ROOT_KEY_PATH=$STEPPATH/persistent-certs/root_ca_key
+ROOT_CRT_PATH=$STEPPATH/persistent-certs/root_ca.crt
+INTERMEDIATE_KEY_PATH=$STEPPATH/persistent-certs/intermediate_ca_key
+INTERMEDIATE_CRT_PATH=$STEPPATH/persistent-certs/intermediate_ca.crt
+STEP_ISSUER_DIR=$STEPPATH/certs/step-issuer-provisioner
+SSH_HOST_DIR=$STEPPATH/certs/ssh-host-provisioner
+
 main() {
-  : "${CLUSTER_NAME:?}" "${STEPPATH:?}" "${NAMESPACE:?}"
-  setup_certificates
+  create_certificates
+  create_secrets
   setup_issuer_provisioner
-  setup_ssh_host_provisioner
+  create_ssh_host_provisioner_key
 }
 
-setup_certificates() {
+create_certificates() {
   printf "bootstrap.sh: Setting up root and intermediate certificates\n" >&2
 
-  local root_ca_is_new=false
-  if [[ ! -e "$STEPPATH/persistent-certs/root_ca_key" ]]; then
-    rm -f "$STEPPATH/persistent-certs/root_ca.crt"
-    printf "bootstrap.sh: Root certificate does not exist on PV, creating now\n" >&2
-    root_ca_is_new=true
-    step certificate create --profile=root-ca \
-      --no-password --insecure \
-      --not-after=87600h \
-      "$CLUSTER_NAME Root" "$STEPPATH/persistent-certs/root_ca.crt" "$STEPPATH/persistent-certs/root_ca_key"
-  else
-    printf "bootstrap.sh: Root certificate exists on PV, skipping creation\n" >&2
-  fi
-
-  if $root_ca_is_new || ! kubectl get -n "$NAMESPACE" secret smallstep-root -o jsonpath='{.data.tls\.crt}' >/dev/null; then
-    printf "bootstrap.sh: Root certificate secret does not exist or has been recreated, creating now\n" >&2
-    kubectl create -n "$NAMESPACE" secret tls smallstep-root \
-      --cert="$STEPPATH/persistent-certs/root_ca.crt" \
-      --key="$STEPPATH/persistent-certs/root_ca_key" || \
-    kubectl replace -n "$NAMESPACE" secret tls smallstep-root \
-      --cert="$STEPPATH/persistent-certs/root_ca.crt" \
-      --key="$STEPPATH/persistent-certs/root_ca_key"
-  else
-    printf "bootstrap.sh: Root certificate secret exists, skipping creation\n" >&2
-  fi
-
-  if $root_ca_is_new || ! kubectl get -n "$NAMESPACE" secret smallstep-intermediate >/dev/null; then
-    printf "bootstrap.sh: Intermediate certificate does not exist or the root has been recreated, creating now\n" >&2
+  if [[ ! -e "$ROOT_KEY_PATH" || ! -e "$INTERMEDIATE_KEY_PATH" ]]; then
+    if [[ ! -e "$ROOT_KEY_PATH" ]]; then
+      printf "bootstrap.sh: Root key does not exist on the PV, creating now\n" >&2
+      rm -f "$ROOT_CRT_PATH"
+      step certificate create --profile=root-ca \
+        --no-password --insecure \
+        --not-after=87600h \
+        "$CLUSTER_NAME Root" "$ROOT_CRT_PATH" "$ROOT_KEY_PATH"
+      printf "bootstrap.sh: Root key is new, creating new intermediate\n" >&2
+    else
+      printf "bootstrap.sh: Root key exists on the PV, skipping creation\n" >&2
+      printf "bootstrap.sh: Intermediate key does not exist on the PV or root has been recreated, creating now\n" >&2
+    fi
+    rm -f "$INTERMEDIATE_KEY_PATH" "$INTERMEDIATE_CRT_PATH"
     step certificate create --profile=intermediate-ca \
       --no-password --insecure \
       --not-after=87600h \
-      --ca="$STEPPATH/persistent-certs/root_ca.crt" --ca-key="$STEPPATH/persistent-certs/root_ca_key" \
-      "$CLUSTER_NAME Intermediate" "$STEPPATH/persistent-certs/intermediate_ca.crt" "$STEPPATH/persistent-certs/intermediate_ca_key"
-    kubectl create -n "$NAMESPACE" secret tls smallstep-intermediate \
-      --cert="$STEPPATH/persistent-certs/intermediate_ca.crt" \
-      --key="$STEPPATH/persistent-certs/intermediate_ca_key" || \
-    kubectl replace -n "$NAMESPACE" secret tls smallstep-intermediate \
-      --cert="$STEPPATH/persistent-certs/intermediate_ca.crt" \
-      --key="$STEPPATH/persistent-certs/intermediate_ca_key"
+      --ca="$ROOT_CRT_PATH" --ca-key="$ROOT_KEY_PATH" \
+      "$CLUSTER_NAME Intermediate" "$INTERMEDIATE_CRT_PATH" "$INTERMEDIATE_KEY_PATH"
   else
-    printf "bootstrap.sh: Intermediate certificate exists, skipping creation\n" >&2
+    printf "bootstrap.sh: Root and intermediate keys exists on the PV, skipping creation\n" >&2
+  fi
+}
+
+create_secrets() {
+  if [[ $(kubectl get -n "$NAMESPACE" secret smallstep-root -o jsonpath='{.data.tls\.crt}' | base64 -d) != $(cat "$ROOT_CRT_PATH") ]]; then
+    printf "bootstrap.sh: Root certificate secret does not exist or does not match the one on the PV, creating now\n" >&2
+    kubectl delete -n "$NAMESPACE" secret smallstep-root 2>/dev/null || true
+    kubectl create -n "$NAMESPACE" secret tls smallstep-root --cert="$ROOT_CRT_PATH" --key="$ROOT_KEY_PATH"
+  else
+    printf "bootstrap.sh: Root certificate secret exists and matches, skipping creation\n" >&2
+  fi
+
+  if [[ $(kubectl get -n "$NAMESPACE" secret smallstep-intermediate -o jsonpath='{.data.tls\.crt}' | base64 -d) != $(cat "$INTERMEDIATE_CRT_PATH") ]]; then
+    printf "bootstrap.sh: Intermediate certificate secret does not exist or the root does not match the one on the PV, creating now\n" >&2
+    kubectl delete -n "$NAMESPACE" secret smallstep-intermediate 2>/dev/null || true
+    kubectl create -n "$NAMESPACE" secret tls smallstep-intermediate --cert="$INTERMEDIATE_CRT_PATH" --key="$INTERMEDIATE_KEY_PATH"
+  else
+    printf "bootstrap.sh: Intermediate certificate secret exists and matches, skipping creation\n" >&2
   fi
 }
 
@@ -58,23 +65,23 @@ setup_issuer_provisioner() {
   printf "bootstrap.sh: Setting up step issuer provisioner\n" >&2
   local jwk_is_new=false
 
-  mkdir "$STEPPATH/certs/step-issuer-provisioner"
-  if ! kubectl get -n "$NAMESPACE" secret step-issuer-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$STEPPATH/certs/step-issuer-provisioner/pub.json"; then
+  mkdir "$STEP_ISSUER_DIR"
+  if ! kubectl get -n "$NAMESPACE" secret step-issuer-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$STEP_ISSUER_DIR/pub.json"; then
     printf "bootstrap.sh: step-issuer provisioner JWK does not exist, creating now\n" >&2
-    rm -f "$STEPPATH/certs/step-issuer-provisioner/pub.json"
+    rm -f "$STEP_ISSUER_DIR/pub.json"
     step crypto jwk create \
       --password-file="$STEPPATH/step-issuer-provisioner-password/password" \
       --use sig \
-      "$STEPPATH/certs/step-issuer-provisioner/pub.json" "$STEPPATH/certs/step-issuer-provisioner/priv.json"
+      "$STEP_ISSUER_DIR/pub.json" "$STEP_ISSUER_DIR/priv.json"
     kubectl create -n "$NAMESPACE" secret generic step-issuer-provisioner \
-      --from-file="$STEPPATH/certs/step-issuer-provisioner/pub.json" \
-      --from-file="$STEPPATH/certs/step-issuer-provisioner/priv.json"
+      --from-file="$STEP_ISSUER_DIR/pub.json" \
+      --from-file="$STEP_ISSUER_DIR/priv.json"
   else
     printf "bootstrap.sh: step-issuer provisioner JWK exists, skipping creation\n" >&2
   fi
 
-  kubectl get stepclusterissuer step-issuer -ojsonpath='{.spec.caBundle}' | base64 -d >"$STEPPATH/certs/step-issuer-provisioner/caBundle.key" || true
-  if $jwk_is_new || ! diff -q "$STEPPATH/persistent-certs/root_ca.crt" "$STEPPATH/certs/step-issuer-provisioner/caBundle.key"; then
+  kubectl get stepclusterissuer step-issuer -ojsonpath='{.spec.caBundle}' | base64 -d >"$STEP_ISSUER_DIR/caBundle.key" || true
+  if $jwk_is_new || ! diff -q "$ROOT_CRT_PATH" "$STEP_ISSUER_DIR/caBundle.key"; then
     printf "bootstrap.sh: StepClusterIssuer does not exist, the provisioner JWK has been recreated, or the caBundle is incorrect, creating now\n" >&2
     kubectl apply -f <(printf -- "apiVersion: certmanager.step.sm/v1beta1
 kind: StepClusterIssuer
@@ -89,28 +96,28 @@ spec:
     passwordRef:
       namespace: smallstep
       name: step-issuer-provisioner-password
-      key: password" "$(base64 -w0 "$STEPPATH/persistent-certs/root_ca.crt")" "$(step crypto jwk thumbprint < "$STEPPATH/certs/step-issuer-provisioner/pub.json")"
+      key: password" "$(base64 -w0 "$ROOT_CRT_PATH")" "$(step crypto jwk thumbprint < "$STEP_ISSUER_DIR/pub.json")"
     )
   else
     printf "bootstrap.sh: StepClusterIssuer exists, skipping creation\n" >&2
   fi
 }
 
-setup_ssh_host_provisioner() {
-  printf "bootstrap.sh: Setting up SSH  provisioner\n" >&2
+create_ssh_host_provisioner_key() {
+  printf "bootstrap.sh: Setting up SSH provisioner\n" >&2
   local jwk_is_new=false
 
-  mkdir "$STEPPATH/certs/ssh-host-provisioner"
-  if ! kubectl get -n "$NAMESPACE" secret ssh-host-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$STEPPATH/certs/ssh-host-provisioner/pub.json"; then
+  mkdir "$SSH_HOST_DIR"
+  if ! kubectl get -n "$NAMESPACE" secret ssh-host-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$SSH_HOST_DIR/pub.json"; then
     printf "bootstrap.sh: ssh-host-provisioner provisioner JWK does not exist, creating now\n" >&2
-    rm -f "$STEPPATH/certs/ssh-host-provisioner/pub.json"
+    rm -f "$SSH_HOST_DIR/pub.json"
     step crypto jwk create \
       --password-file="$STEPPATH/ssh-host-provisioner-password/password" \
       --use sig \
-      "$STEPPATH/certs/ssh-host-provisioner/pub.json" "$STEPPATH/certs/ssh-host-provisioner/priv.json"
+      "$SSH_HOST_DIR/pub.json" "$SSH_HOST_DIR/priv.json"
     kubectl create -n "$NAMESPACE" secret generic ssh-host-provisioner \
-      --from-file="$STEPPATH/certs/ssh-host-provisioner/pub.json" \
-      --from-file="$STEPPATH/certs/ssh-host-provisioner/priv.json"
+      --from-file="$SSH_HOST_DIR/pub.json" \
+      --from-file="$SSH_HOST_DIR/priv.json"
   else
     printf "bootstrap.sh: ssh-host provisioner JWK exists, skipping creation\n" >&2
   fi
--- manifests/smallstep/config/setup-config.sh
+++ manifests/smallstep/config/setup-config.sh
@@ -3,17 +3,45 @@ set -Eeo pipefail
 
 main() {
   : "${STEPPATH:?}" "${STEP_CA_DOMAIN:?}"
-  apk add --update --no-cache jq
+  apk add --update --no-cache jq moreutils
   jq --arg domain "$STEP_CA_DOMAIN" '.dnsNames+=[$domain]' "$STEPPATH/config-ro/ca.json" > "$STEPPATH/config/ca.json"
-  printf "setup-config.sh: Adding step-issuer to ca.json\n" >&2
-  step ca provisioner add step-issuer --type JWK \
+
+  local step_issuer_config ssh_host_config step_issuer ssh_host
+  info "Storing and then removing step-issuer & ssh-host provisioners from ca.json"
+  # 1. Store the provisioner configs from ca.json
+  step_issuer_config=$(jq '.authority.provisioners[] | select(.name=="step-issuer")' "$STEPPATH/config/ca.json")
+  ssh_host_config=$(jq '.authority.provisioners[] | select(.name=="ssh-host")' "$STEPPATH/config/ca.json")
+  # 2. Remove the provisioner configs from ca.json
+  jq 'del(.authority.provisioners[] | select(.name=="step-issuer" or .name=="ssh-host"))' "$STEPPATH/config/ca.json" | sponge "$STEPPATH/config/ca.json"
+  info "Adding step-issuer provisioner key in ca.json"
+  # 3. Add the provisioner keys through `step ca provisioner add` to ca.json
+  step ca provisioner add step-issuer \
     --public-key="$STEPPATH/certs/step-issuer-provisioner/pub.json" \
     --private-key="$STEPPATH/certs/step-issuer-provisioner/priv.json" \
     --password-file="$STEPPATH/step-issuer-provisioner-password/password"
-  step ca provisioner add ssh-host --type JWK \
+  info "Adding ssh-host provisioner key in ca.json"
+  step ca provisioner add ssh-host \
     --public-key="$STEPPATH/certs/ssh-host-provisioner/pub.json" \
     --private-key="$STEPPATH/certs/ssh-host-provisioner/priv.json" \
     --password-file="$STEPPATH/ssh-host-provisioner-password/password"
+  info "Merging new provisioner keys with stored provisioner config from ca.json"
+  # 4. Read the added keys from ca.json and apply them to the stored configs
+  step_issuer=$(jq --argjson key "$(jq '.authority.provisioners[] | select(.name=="step-issuer")' "$STEPPATH/config/ca.json")" \
+    '.key=$key.key | .encryptedKey=$key.encryptedKey' <<<"$step_issuer_config")
+  ssh_host=$(jq --argjson key "$(jq '.authority.provisioners[] | select(.name=="ssh-host")' "$STEPPATH/config/ca.json")" \
+    '.key=$key.key | .encryptedKey=$key.encryptedKey' <<<"$ssh_host_config")
+  info "Replacing added provisioners with merged config"
+  # 5. Remove the added provisioners configs from ca.json
+  jq 'del(.authority.provisioners[] | select(.name=="step-issuer" or .name=="ssh-host"))' "$STEPPATH/config/ca.json" | sponge "$STEPPATH/config/ca.json"
+  # 6. Add the updated provisioner configs to ca.json
+  jq --argjson step_issuer "$step_issuer" --argjson ssh_host "$ssh_host" \
+    '.authority.provisioners += [$step_issuer, $ssh_host]' "$STEPPATH/config/ca.json" | sponge "$STEPPATH/config/ca.json"
+}
+
+info() {
+  local tpl=$1; shift
+  # shellcheck disable=2059
+  printf "setup-config.sh: $tpl\n" "$@" >&2
 }
 
 main "$@"
