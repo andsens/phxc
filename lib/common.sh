91af040927ae692ff62efe7341b59d0eaea95aa1
Anders Ingemann <aim@orbit.online>
2024-04-28 12:19:19 +0200

    common.sh: Add $HOME/.local/bin to $PATH when sudo'ing

--- lib/common.sh
+++ lib/common.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck source-path=..
 
+PATH=$("$PKGROOT/.upkg/.bin/path_prepend" "$PKGROOT/.upkg/.bin")
+[[ -z $SUDO_USER ]] || PATH=$(path_prepend "$(eval "echo ~$SUDO_USER")/.local/bin")
+
 source "$PKGROOT/.upkg/orbit-online/records.sh/records.sh"
 source "$PKGROOT/.upkg/orbit-online/collections.sh/collections.sh"
 source "$PKGROOT/lib/mount.sh"
