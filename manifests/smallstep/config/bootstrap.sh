a9d5864628e714981add57f7f7b0f7042aaa590f
Anders Ingemann <aim@orbit.online>
2024-04-28 09:03:02 +0200

    smallstep: Don't add root key to kubernetes secrets

--- manifests/smallstep/config/bootstrap.sh
+++ manifests/smallstep/config/bootstrap.sh
@@ -17,21 +17,22 @@ main() {
 }
 
 create_certificates() {
-  printf "bootstrap.sh: Setting up root and intermediate certificates\n" >&2
-
-  if [[ ! -e "$ROOT_KEY_PATH" || ! -e "$INTERMEDIATE_KEY_PATH" ]]; then
-    if [[ ! -e "$ROOT_KEY_PATH" ]]; then
-      printf "bootstrap.sh: Root key does not exist on the PV, creating now\n" >&2
-      rm -f "$ROOT_CRT_PATH"
-      step certificate create --profile=root-ca \
-        --no-password --insecure \
-        --not-after=87600h \
-        "$CLUSTER_NAME Root" "$ROOT_CRT_PATH" "$ROOT_KEY_PATH"
-      printf "bootstrap.sh: Root key is new, creating new intermediate\n" >&2
-    else
-      printf "bootstrap.sh: Root key exists on the PV, skipping creation\n" >&2
-      printf "bootstrap.sh: Intermediate key does not exist on the PV or root has been recreated, creating now\n" >&2
-    fi
+  info "Setting up root and intermediate certificates"
+  local root_is_new=false
+  if [[ ! -e "$ROOT_KEY_PATH" || ! -e "$ROOT_CRT_PATH" ]]; then
+    info "Root key and/or cert do not exist on the PV, creating now"
+    rm -f "$ROOT_CRT_PATH"
+    step certificate create --profile=root-ca \
+      --no-password --insecure \
+      --not-after=87600h \
+      "$CLUSTER_NAME Root" "$ROOT_CRT_PATH" "$ROOT_KEY_PATH"
+    root_is_new=true
+    info "Root key and cert do are new, creating new intermediate"
+  else
+    info "Root key and cert exists on the PV, skipping creation"
+  fi
+  if $root_is_new || [[ ! -e "$INTERMEDIATE_KEY_PATH" || ! -e "$INTERMEDIATE_CRT_PATH" ]]; then
+    info "Intermediate key and/or cert do not exist on the PV or the root has been recreated, creating now"
     rm -f "$INTERMEDIATE_KEY_PATH" "$INTERMEDIATE_CRT_PATH"
     step certificate create --profile=intermediate-ca \
       --no-password --insecure \
@@ -39,35 +40,36 @@ create_certificates() {
       --ca="$ROOT_CRT_PATH" --ca-key="$ROOT_KEY_PATH" \
       "$CLUSTER_NAME Intermediate" "$INTERMEDIATE_CRT_PATH" "$INTERMEDIATE_KEY_PATH"
   else
-    printf "bootstrap.sh: Root and intermediate keys exists on the PV, skipping creation\n" >&2
+    info "Intermediate key & cert exists on the PV, skipping creation"
   fi
 }
 
 create_secrets() {
+  info "Setting up step issuer provisioner"
   if [[ $(kubectl get -n "$NAMESPACE" secret smallstep-root -o jsonpath='{.data.tls\.crt}' | base64 -d) != $(cat "$ROOT_CRT_PATH") ]]; then
-    printf "bootstrap.sh: Root certificate secret does not exist or does not match the one on the PV, creating now\n" >&2
+    info "Root certificate secret does not exist or does not match the one on the PV, creating now"
     kubectl delete -n "$NAMESPACE" secret smallstep-root 2>/dev/null || true
-    kubectl create -n "$NAMESPACE" secret tls smallstep-root --cert="$ROOT_CRT_PATH" --key="$ROOT_KEY_PATH"
+    kubectl create -n "$NAMESPACE" secret generic smallstep-root --from-file=tls.crt="$ROOT_CRT_PATH"
   else
-    printf "bootstrap.sh: Root certificate secret exists and matches, skipping creation\n" >&2
+    info "Root certificate secret exists and matches, skipping creation"
   fi
 
   if [[ $(kubectl get -n "$NAMESPACE" secret smallstep-intermediate -o jsonpath='{.data.tls\.crt}' | base64 -d) != $(cat "$INTERMEDIATE_CRT_PATH") ]]; then
-    printf "bootstrap.sh: Intermediate certificate secret does not exist or the root does not match the one on the PV, creating now\n" >&2
+    info "Intermediate certificate secret does not exist or the root does not match the one on the PV, creating now"
     kubectl delete -n "$NAMESPACE" secret smallstep-intermediate 2>/dev/null || true
     kubectl create -n "$NAMESPACE" secret tls smallstep-intermediate --cert="$INTERMEDIATE_CRT_PATH" --key="$INTERMEDIATE_KEY_PATH"
   else
-    printf "bootstrap.sh: Intermediate certificate secret exists and matches, skipping creation\n" >&2
+    info "Intermediate certificate secret exists and matches, skipping creation"
   fi
 }
 
 setup_issuer_provisioner() {
-  printf "bootstrap.sh: Setting up step issuer provisioner\n" >&2
+  info "Setting up step issuer provisioner"
   local jwk_is_new=false
 
   mkdir "$STEP_ISSUER_DIR"
   if ! kubectl get -n "$NAMESPACE" secret step-issuer-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$STEP_ISSUER_DIR/pub.json"; then
-    printf "bootstrap.sh: step-issuer provisioner JWK does not exist, creating now\n" >&2
+    info "step-issuer provisioner JWK does not exist, creating now"
     rm -f "$STEP_ISSUER_DIR/pub.json"
     step crypto jwk create \
       --password-file="$STEPPATH/step-issuer-provisioner-password/password" \
@@ -77,12 +79,12 @@ setup_issuer_provisioner() {
       --from-file="$STEP_ISSUER_DIR/pub.json" \
       --from-file="$STEP_ISSUER_DIR/priv.json"
   else
-    printf "bootstrap.sh: step-issuer provisioner JWK exists, skipping creation\n" >&2
+    info "step-issuer provisioner JWK exists, skipping creation"
   fi
 
   kubectl get stepclusterissuer step-issuer -ojsonpath='{.spec.caBundle}' | base64 -d >"$STEP_ISSUER_DIR/caBundle.key" || true
   if $jwk_is_new || ! diff -q "$ROOT_CRT_PATH" "$STEP_ISSUER_DIR/caBundle.key"; then
-    printf "bootstrap.sh: StepClusterIssuer does not exist, the provisioner JWK has been recreated, or the caBundle is incorrect, creating now\n" >&2
+    info "StepClusterIssuer does not exist, the provisioner JWK has been recreated, or the caBundle is incorrect, creating now"
     kubectl apply -f <(printf -- "apiVersion: certmanager.step.sm/v1beta1
 kind: StepClusterIssuer
 metadata:
@@ -99,17 +101,17 @@ spec:
       key: password" "$(base64 -w0 "$ROOT_CRT_PATH")" "$(step crypto jwk thumbprint < "$STEP_ISSUER_DIR/pub.json")"
     )
   else
-    printf "bootstrap.sh: StepClusterIssuer exists, skipping creation\n" >&2
+    info "StepClusterIssuer exists, skipping creation"
   fi
 }
 
 create_ssh_host_provisioner_key() {
-  printf "bootstrap.sh: Setting up SSH provisioner\n" >&2
+  info "Setting up SSH host provisioner"
   local jwk_is_new=false
 
   mkdir "$SSH_HOST_DIR"
   if ! kubectl get -n "$NAMESPACE" secret ssh-host-provisioner -o jsonpath='{.data.pub\.json}' | base64 -d >"$SSH_HOST_DIR/pub.json"; then
-    printf "bootstrap.sh: ssh-host-provisioner provisioner JWK does not exist, creating now\n" >&2
+    info "ssh-host-provisioner provisioner JWK does not exist, creating now"
     rm -f "$SSH_HOST_DIR/pub.json"
     step crypto jwk create \
       --password-file="$STEPPATH/ssh-host-provisioner-password/password" \
@@ -119,8 +121,14 @@ create_ssh_host_provisioner_key() {
       --from-file="$SSH_HOST_DIR/pub.json" \
       --from-file="$SSH_HOST_DIR/priv.json"
   else
-    printf "bootstrap.sh: ssh-host provisioner JWK exists, skipping creation\n" >&2
+    info "ssh-host provisioner JWK exists, skipping creation"
   fi
+}
+
+info() {
+  local tpl=$1; shift
+  # shellcheck disable=2059
+  printf "setup-config.sh: $tpl\n" "$@" >&2
 }
 
 main "$@"
