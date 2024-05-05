51995c544c459f9b27feaf5608858130dace5d15
Anders Ingemann <aim@orbit.online>
2024-05-05 22:48:45 +0200

    Remove apply.sh

--- manifests/redis/apply.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck source-path=../../
-set -Eeo pipefail
-until [[ -e $PKGROOT/upkg.json || $PKGROOT = '/' ]]; do PKGROOT=$(dirname "${PKGROOT:-$(realpath "${BASH_SOURCE[0]}")}"); done
-source "$PKGROOT/.upkg/orbit-online/records.sh/records.sh"
-
-MANIFEST_ROOT=$(dirname "${BASH_SOURCE[0]}")
-kustomize build "$MANIFEST_ROOT" | kpt live apply --context k3s - "$@"
