b06b400d27bbcb387e0c22b0110b2b9779589c06
Anders Ingemann <aim@orbit.online>
2024-05-25 20:40:57 +0200

    PXE: Load kernel via http, autogen autoexec.ipxe

--- workloads/pxe/commands/tftpd.sh
+++ workloads/pxe/commands/tftpd.sh
@@ -3,12 +3,18 @@
 set -Eeo pipefail; shopt -s inherit_errexit
 PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../..")
 
+CONFIG_TPL=/var/lib/home-cluster/workloads/pxe/config/autoexec.ipxe
+CONFIG=/tftp/config/autoexec.ipxe
+
 main() {
   source "$PKGROOT/lib/common.sh"
+
+  envsubst <$CONFIG_TPL >$CONFIG
   info "Starting tftpd"
   exec /usr/sbin/in.tftpd --foreground \
   --user tftp \
   --address :69 \
+  --map-file /var/lib/home-cluster/workloads/pxe/config/map-file \
   --secure /tftp
 }
 
