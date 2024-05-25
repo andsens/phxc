a26174dde079547b6ae58225bb28f441d22ee880
Anders Ingemann <aim@orbit.online>
2024-05-25 11:16:18 +0200

    Make create-kube-apiserver-client-ca-secret.sh dependencyless again

--- workloads/smallstep/commands/create-kube-apiserver-client-ca-secret.sh
+++ workloads/smallstep/commands/create-kube-apiserver-client-ca-secret.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 # shellcheck source-path=../../..
 set -Eeo pipefail; shopt -s inherit_errexit
-PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../..")
-source "$PKGROOT/lib/common.sh"
 
 : "${NAMESPACE:?}"
+
+# Must be dependencyless, because it is called from docker-registry setup as well
 
 main() {
   local cert_path=$1
   cert=$(cat "$cert_path")
   if [[ $(kubectl get -n "$NAMESPACE" secret kube-apiserver-client-ca -o jsonpath='{.data.tls\.crt}' | base64 -d) != "$cert" ]]; then
-    info "kube-apiserver client CA secret validation failed, (re-)creating now"
+    printf "create-kube-apiserver-client-ca-secret.sh: kube-apiserver client CA secret validation failed, (re-)creating now" >&2
     kubectl delete -n "$NAMESPACE" secret kube-apiserver-client-ca 2>/dev/null || true
     kubectl create -n "$NAMESPACE" secret generic kube-apiserver-client-ca --from-file=tls.crt="$cert_path"
   else
-    info "kube-apiserver client CA secret validation succeeded"
+    printf "create-kube-apiserver-client-ca-secret.sh: kube-apiserver client CA secret validation succeeded" >&2
   fi
 }
 
