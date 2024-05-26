498256c594971304f671f17856d29911d99b6c17
Anders Ingemann <aim@orbit.online>
2024-05-26 23:18:13 +0200

    Move some systemd service disables

--- workloads/bootstrap/task.d/10-os.sh
+++ workloads/bootstrap/task.d/10-os.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 PACKAGES+=(systemd adduser)
+
+os() {
+  systemctl disable fstrim e2scrub_all e2scrub_reap dpkg-db-backup apt-daily.timer apt-daily-upgrade.timer
+  systemctl mask apt-daily.service apt-daily-upgrade.service
+  # TODO: journalctl max size
+}
--- workloads/bootstrap/task.d/50-root.sh
+++ workloads/bootstrap/task.d/50-root.sh
@@ -5,6 +5,5 @@ PACKAGES+=(overlayroot)
 root() {
   cp_tpl /etc/overlayroot.conf
 
-  systemctl disable fstrim e2scrub_all e2scrub_reap dpkg-db-backup apt-daily.timer apt-daily-upgrade.timer
-  systemctl mask apt-daily.service
+  systemctl disable fstrim e2scrub_all e2scrub_reap
 }
