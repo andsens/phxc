56d08ff55caeac1b030c6bc0eb66620393d909a7
Anders Ingemann <aim@orbit.online>
2024-06-05 22:16:14 +0200

    Get container building working

--- workloads/bootstrap/containers/node/tasks.d/25-cachedev.sh
+++ workloads/bootstrap/containers/node/tasks.d/25-cachedev.sh
@@ -3,4 +3,6 @@
 cachedev() {
   mkdir -p /var/lib/rancher/k3s/agent/containerd
   cp_tpl --raw --chmod=0755 /etc/initramfs-tools/scripts/init-bottom/cachedev
+  cp_tpl --raw --chmod=0755 /etc/systemd/system/mk-cache-dirs.service
+  cp_tpl --raw --chmod=0755 /etc/systemd/system/var-lib-rancher-k3s-agent-containerd.mount
 }
