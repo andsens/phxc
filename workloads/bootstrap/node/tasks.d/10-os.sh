845495e24e24b4166e513ba1a16d08a92218ed76
Anders Ingemann <aim@orbit.online>
2024-06-30 17:15:26 +0200

    bootstrap: Remove duplicated systemctl disable

--- workloads/bootstrap/node/tasks.d/10-os.sh
+++ workloads/bootstrap/node/tasks.d/10-os.sh
@@ -3,6 +3,6 @@
 PACKAGES+=(systemd adduser)
 
 os() {
-  systemctl disable fstrim e2scrub_all e2scrub_reap dpkg-db-backup apt-daily.timer apt-daily-upgrade.timer
+  systemctl disable dpkg-db-backup apt-daily.timer apt-daily-upgrade.timer
   systemctl mask apt-daily.service apt-daily-upgrade.service
 }
