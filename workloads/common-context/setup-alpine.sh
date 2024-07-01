ba6f28c212e344615760770ad7d33e063d9cae57
Anders Ingemann <aim@orbit.online>
2024-07-02 00:15:56 +0200

    upkg: bump upkg & records.sh. Remove unneeded global deps in containers

--- workloads/common-context/setup-alpine.sh
+++ workloads/common-context/setup-alpine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeo pipefail; shopt -s inherit_errexit
 
-apk add jq gettext py3-virtualenv perl-utils
+apk add jq gettext py3-virtualenv
 virtualenv /usr/local/lib/yq
 /usr/local/lib/yq/bin/pip3 install yq
 ln -s /usr/local/lib/yq/bin/yq /usr/local/bin/yq
@@ -9,11 +9,7 @@ ln -s /usr/local/lib/yq/bin/yq /usr/local/bin/yq
 temp=$(mktemp)
 # shellcheck disable=SC2064
 trap "rm \"$temp\"" EXIT
-wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.26.5/upkg-install.tar.gz"
-shasum -a 256 -c <(echo "478ea3b01d58e2adf32579c2b1abfb257b24d17464367050e254063e42143b12  $temp")
+wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.27.0/upkg-install.tar.gz"
+sha256sum -c <(echo "a27e0178a6ee420a8cd8273125a139394845049f3d0667c91d63a53b31756dec  $temp")
 tar xzC /usr/local -f "$temp"
-upkg add -gp docopt-lib-v2.0.1 "https://github.com/andsens/docopt.sh/releases/download/v2.0.1/docopt-lib.sh.tar.gz" 539053da8b3063921b8889dbe752279e3a215d8fa3e2550d6521e094981f26a2
-upkg add -g "https://github.com/orbit-online/trap.sh/releases/download/v1.1.1/trap.sh.tar.gz" bdc50e863a44866a6d92c11570d8e255f95d65747128e6f262985452e7359bb4
-upkg add -g "https://github.com/orbit-online/records.sh/releases/download/v1.0.0/records.sh.tar.gz" 0469f15f5b689cb29b30f2a3b233c61856eb209fc50c74ef141120bbf70697f1
-upkg add -g "https://github.com/orbit-online/path-tools/releases/download/v1.0.0/path-tools.tar.gz" 2ae2a98714aa81e2142b749dac9ecdb61e050e66bf8bd33aef0fccb2ce66c84b
-upkg add -g "https://github.com/orbit-online/collections.sh/releases/download/v1.0.0/collections.sh.tar.gz" ca741323c2bd77f547fa9aea41050d85dfc5f1ce3ff42f73b7a12f7c90b9be2e
+upkg add -g "https://github.com/orbit-online/records.sh/releases/download/v1.0.2/records.sh.tar.gz" 201977ecc5fc9069d8eff12ba6adc9ce1286ba66c9aeee19184e26185cc6ef63
