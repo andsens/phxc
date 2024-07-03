eef3922e075e2b6cbc5f7d7e5689c6a709982762
Anders Ingemann <aim@orbit.online>
2024-07-03 08:39:03 +0200

    common-context: Use prepopulated upkg.json for global installs

--- workloads/common-context/setup-alpine.sh
+++ workloads/common-context/setup-alpine.sh
@@ -12,4 +12,9 @@ trap "rm \"$temp\"" EXIT
 wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.27.0/upkg-install.tar.gz"
 sha256sum -c <(echo "a27e0178a6ee420a8cd8273125a139394845049f3d0667c91d63a53b31756dec  $temp")
 tar xzC /usr/local -f "$temp"
-upkg add -g "https://github.com/orbit-online/records.sh/releases/download/v1.0.2/records.sh.tar.gz" 201977ecc5fc9069d8eff12ba6adc9ce1286ba66c9aeee19184e26185cc6ef63
+upkg_json=$(jq --argjson common "$(cat "$(dirname "${BASH_SOURCE[0]}")/common.upkg.json")" \
+  '.dependencies += $common.dependencies' \
+  /usr/local/lib/upkg/upkg.json
+)
+printf "%s\n" "$upkg_json" >/usr/local/lib/upkg/upkg.json
+(cd /usr/local/lib/upkg; upkg install)
