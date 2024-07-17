e3e1aa7380e74f5e575816be2afd3bfd199700d4
Anders Ingemann <aim@orbit.online>
2024-07-17 17:36:53 +0200

    node: Bump upkg to v0.28.0 and use os/arch filter

--- workloads/common-context/setup-debian.sh
+++ workloads/common-context/setup-debian.sh
@@ -9,8 +9,8 @@ rm -rf /var/cache/apt/lists/*
 temp=$(mktemp)
 # shellcheck disable=SC2064
 trap "rm \"$temp\"" EXIT
-wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.27.0/upkg-install.tar.gz"
-sha256sum -c <(echo "a27e0178a6ee420a8cd8273125a139394845049f3d0667c91d63a53b31756dec  $temp")
+wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.28.0/upkg-install.tar.gz"
+sha256sum -c <(echo "abd498bf0b50dbda1e115546a191c781796b7af4e597f3c3a29ba210c8352ccb  $temp")
 tar xzC /usr/local -f "$temp"
 upkg_json=$(jq --argjson common "$(cat "$(dirname "${BASH_SOURCE[0]}")/common.upkg.json")" \
   '.dependencies += $common.dependencies' \
