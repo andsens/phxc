f8d6fe0f7c6a6966737ab44d934513c8fca29650
Anders Ingemann <aim@orbit.online>
2024-08-15 20:36:05 +0200

    node: Refactor registry to be able to serve images, simplifying the startup setup

--- workloads/node/scripts/registry/__init__.py
+++ workloads/node/scripts/registry/__init__.py
@@ -22,22 +22,26 @@ app.config['DEBUG'] = os.getenv('LOGLEVEL', 'INFO').upper() == 'DEBUG'
 def root_get():
   flask.abort(405)
 
-@app.route('/node-config/<mac>.json', methods=['GET'])
-def get_node_config(mac):
-  if re.match(r'^([0-9a-f]{2}-){5}[0-9a-f]{2}$', mac) is None:
-    flask.abort(400)
-  with open(os.path.join(app.config['root'], f'node-state/{mac}.json'), 'r') as h:
+@app.route('/images/<path:image_path>')
+def send_report(image_path):
+  if not app.config['enable_images']:
+    flask.abort(500)
+  return flask.send_file(os.path.join(app.config['root'], 'images', image_path))
+
+@app.route('/registry/node-config/<path:node_config_path>', methods=['GET'])
+def get_node_config(node_config_path):
+  with open(os.path.join(app.config['root'], 'node-config', node_config_path), 'r') as h:
     try:
-      return json.load(h)
+      raw_json = h.read()
+      json.loads(raw_json)  # Make sure we are sending back valid JSON
+      return raw_json
     except Exception as e:
       log.error(e)
       flask.abort(500)
 
-@app.route('/node-state/<mac>.json', methods=['PUT'])
-def put_node_state(mac):
-  if re.match(r'^([0-9a-f]{2}-){5}[0-9a-f]{2}$', mac) is None:
-    flask.abort(400)
-  with open(os.path.join(app.config['root'], f'node-state/{mac}.json'), 'w') as h:
+@app.route('/registry/node-state/<path:node_state_path>', methods=['PUT'])
+def put_node_state(node_state_path):
+  with open(os.path.join(app.config['root'], 'node-state', node_state_path), 'w') as h:
     try:
       node_state = json.loads(flask.request.get_data())
       h.write(json.dumps(node_state, indent=2))
@@ -46,12 +50,13 @@ def put_node_state(mac):
       flask.abort(400)
   return {'result': 'OK'}
 
-@app.route('/attest', methods=['POST'])
+@app.route('/registry/attest', methods=['POST'])
 def post_attest():
   verify_attestation(flask.request.files)
 
-def gunicorn(root):
+def gunicorn(root, enable_images=False):
   app.config['root'] = root
+  app.config['enable_images'] = enable_images
   return app
 
 if __name__ == '__main__':
