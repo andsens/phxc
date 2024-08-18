0c2a52c0abc1caafd953bb221f39d36f2ceb9530
Anders Ingemann <aim@orbit.online>
2024-08-18 18:59:32 +0200

    node: Disable more apt stuff

--- workloads/node/bootstrap/tasks.d/10-os.sh
+++ workloads/node/bootstrap/tasks.d/10-os.sh
@@ -6,10 +6,12 @@ os() {
   systemctl disable \
     dpkg-db-backup \
     apt-daily.timer \
-    apt-daily-upgrade.timer \
     apt-daily.service \
+    apt-daily-upgrade.timer \
     apt-daily-upgrade.service
   systemctl mask \
+    apt-daily.timer \
     apt-daily.service \
+    apt-daily-upgrade.timer \
     apt-daily-upgrade.service
 }
