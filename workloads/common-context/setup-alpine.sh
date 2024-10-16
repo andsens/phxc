2ca8b604c67e51838c8600a829c7a6f76bf4ec49
Anders Ingemann <aim@orbit.online>
2024-10-16 19:40:34 +0200

    common-context: Update upkg to v0.28.2

--- workloads/common-context/setup-alpine.sh
+++ workloads/common-context/setup-alpine.sh
@@ -9,8 +9,8 @@ ln -s /usr/local/lib/yq/bin/yq /usr/local/bin/yq
 temp=$(mktemp)
 # shellcheck disable=SC2064
 trap "rm \"$temp\"" EXIT
-wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.28.0/upkg-install.tar.gz"
-sha256sum -c <(echo "abd498bf0b50dbda1e115546a191c781796b7af4e597f3c3a29ba210c8352ccb  $temp")
+wget -qO"$temp" "https://github.com/orbit-online/upkg/releases/download/v0.28.2/upkg-install.tar.gz"
+sha256sum -c <(echo "4a2956232b059b11395b9d575a817233a67a56528217a576e9016cbaa62f007c  $temp")
 tar xzC /usr/local -f "$temp"
 upkg_json=$(jq --argjson common "$(cat "$(dirname "${BASH_SOURCE[0]}")/common.upkg.json")" \
   '.dependencies += $common.dependencies' \
