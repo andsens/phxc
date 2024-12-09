0475616b5b19eea73717c70420d49d602b7905cf
Anders Ingemann <aim@orbit.online>
2024-12-09 08:43:31 +0100

    Get try-reboot working

--- workloads/node/bootstrap/tasks.d/25-update-boot.sh
+++ workloads/node/bootstrap/tasks.d/25-update-boot.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+case $VARIANT in
+  amd64) PACKAGES+=(systemd-boot) ;;
+  arm64) PACKAGES+=(systemd-boot) ;;
+  rpi*) ;;
+  *) printf "Unknown variant: %s\n" "$VARIANT" >&2; return 1 ;;
+esac
+
+rpi() {
+  :
+}
+
 update_boot() {
   install_sd_unit -e update-boot/update-boot.service
   install_sd_unit -e update-boot/update-boot.timer
