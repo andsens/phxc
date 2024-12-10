83b09b7ac15e375fc27316a5f698b904266a7dd2
Anders Ingemann <aim@orbit.online>
2024-12-10 19:18:30 +0100

    Move get-config back to node root/usr/local/bin

--- workloads/node/bootstrap/tasks.d/40-config.sh
+++ workloads/node/bootstrap/tasks.d/40-config.sh
@@ -6,6 +6,7 @@ PACKAGES+=(
 )
 
 config() {
+  chmod 0755 /usr/local/bin/get-config
   mkdir /etc/phxc
   ln -sf ../run/machine-id /etc/machine-id
   ln -sf ../../../run/machine-id /var/lib/dbus/machine-id
