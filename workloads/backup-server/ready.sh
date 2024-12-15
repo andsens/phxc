a1727161672dd1ab48534270109be63bc0a5f5e6
Anders Ingemann <aim@orbit.online>
2024-12-15 11:33:32 +0100

    Add backup-server workload, use for bootstrapping trust for now
    The intention is to also serve a tarball with everything needed
    to rebuild a cluster with the same private keys and certificates

---
+++ workloads/backup-server/ready.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# shellcheck source-path=../..
+set -Eeo pipefail; shopt -s inherit_errexit
+source "$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")/lib/resource-ready.sh"
+is_ready() { deployment_ready phxc backup-server; }
+check_ready "$@"
