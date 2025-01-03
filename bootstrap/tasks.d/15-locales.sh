57d152208cd5c3aea3006e61a471590f95c8dd84
Anders Ingemann <aim@orbit.online>
2025-01-03 13:49:13 +0100

    Fix locales setup in debug

--- bootstrap/tasks.d/15-locales.sh
+++ bootstrap/tasks.d/15-locales.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-PACKAGES+=(locales)
+if $DEBUG; then
+  PACKAGES+=(locales)
+fi
 
 locales() {
   rm /etc/timezone /etc/localtime
@@ -8,7 +10,7 @@ locales() {
 tzdata tzdata/Zones/Europe select Copenhagen"
   dpkg-reconfigure --frontend noninteractive tzdata
   if $DEBUG; then
-    debconf-set-selections - <<<"locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8
+    debconf-set-selections - <<<"locales locales/default_environment_locale select en_US.UTF-8
 locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8"
     dpkg-reconfigure --frontend noninteractive locales
   fi
