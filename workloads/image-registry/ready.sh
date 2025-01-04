ec65044d6bf189ef250b87e8dc7cb4cd9744a99a
Anders Ingemann <aim@orbit.online>
2025-01-04 20:37:33 +0100

    Improve readiness check for image-registry

--- workloads/image-registry/ready.sh
+++ workloads/image-registry/ready.sh
@@ -2,5 +2,8 @@
 # shellcheck source-path=../..
 set -Eeo pipefail; shopt -s inherit_errexit
 source "$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")/lib/resource-ready.sh"
-is_ready() { deployment_ready phxc image-registry; }
+is_ready() {
+  deployment_ready phxc image-registry
+  curl -sfk https://image-registry.phxc.svc.cluster.local:8020/health &>/dev/null
+}
 check_ready "$@"
