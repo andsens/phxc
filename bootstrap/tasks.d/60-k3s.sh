3e6b4c3afe46730b2a4774a0d72beb5714f3a96d
Anders Ingemann <aim@orbit.online>
2025-01-11 16:24:51 +0100

    Remove longhorn workload by relying on hostPath mounts
    smallstep does not need db persistence for anything except revocation,
    which doesn't really makes sense anyways because we have disabled renewal.
    
    If apt-cacher-ng happens to be schedule on another host, let it. It's
    not really a big deal to have the cache on more than one machine.
    
    We force the image-registry to run on the control-plane. This will
    be a problem once we support multiple masters

--- bootstrap/tasks.d/60-k3s.sh
+++ bootstrap/tasks.d/60-k3s.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-PACKAGES+=(
-  open-iscsi nfs-common # longhorn
-)
-
 k3s() {
   chmod 0750 /var/lib/kubelet
 }
--- workloads/snapshots/ready.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck source-path=../..
-set -Eeo pipefail; shopt -s inherit_errexit
-source "$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..")/lib/resource-ready.sh"
-is_ready() { deployment_ready kube-system snapshot-controller; }
-check_ready "$@"
