929f357f1a2da465ae46f021386067e8ac59c538
Anders Ingemann <aim@orbit.online>
2025-02-14 22:49:36 +0100

    rpi: Move kernel & firmware setup to boot task, fix rpi-eeprom-update

--- bootstrap/tasks.d/30-rpi.sh
+++ bootstrap/tasks.d/30-rpi.sh
@@ -35,46 +35,10 @@ EOF
 fi
 
 rpi() {
-  [[ $VARIANT = rpi* ]] || return 0
-
-  local kver kerneltmp rpi_suffix
-  kerneltmp=$(mktemp -d)
-  wget -qO>(tar -xzC "$kerneltmp" --strip-components=1) \
-    "https://github.com/raspberrypi/firmware/archive/5eaa42401e9625415934c97baae5a8d65933768c.tar.gz"
-  case "$VARIANT" in
-    rpi2|rpi3) rpi_suffix=7 ;;
-    rpi4) rpi_suffix=8 ;;
-    rpi5) rpi_suffix=_2712 ;;
-  esac
-  uname_path="$kerneltmp/extra/uname_string${rpi_suffix}"
-  kver=$(grep -o '^Linux version [^ ]\+' "$uname_path" | cut -d' ' -f3)
-  mkdir /usr/lib/modules
-  cp -r "$kerneltmp/modules/$kver" "/usr/lib/modules/$kver"
-  cp "$kerneltmp/boot/kernel${rpi_suffix}.img" "/boot/vmlinuz-$kver"
-  ln -s "boot/vmlinuz-$kver" /vmlinuz
-  ln -s "boot/vmlinuz-$kver" /vmlinuz.old
-
-  mkdir /boot/firmware
-  cp -r "$kerneltmp/boot/overlays" /boot/firmware/overlays
-  case "$VARIANT" in
-    rpi2)
-      cp "$kerneltmp/boot/start_x.elf" \
-          "$kerneltmp/boot"/bcm*-rpi-2* \
-          /boot/firmware
-      ;;
-    rpi3)
-      cp "$kerneltmp/boot/start_x.elf" \
-          "$kerneltmp/boot"/bcm2710-rpi-3* \
-          /boot/firmware
-      ;;
-    rpi4)
-      cp "$kerneltmp/boot/start4x.elf" \
-          "$kerneltmp/boot"/bcm2711* \
-          /boot/firmware
-      ;;
-    rpi5)
-      cp "$kerneltmp/boot"/bcm2712* /boot/firmware
-      ;;
-  esac
-  rm -rf "$kerneltmp"
+  if [[ $VARIANT = rpi* ]]; then
+    :
+  else
+    rm -rf /etc/systemd/system/rpi-eeprom-update.service.d \
+           /etc/default/rpi-eeprom-update
+  fi
 }
--- bootstrap/tasks.d/95-boot.sh
+++ bootstrap/tasks.d/95-boot.sh
@@ -39,6 +39,7 @@ boot() {
   if [[ $VARIANT = rpi* ]]; then
     rm /etc/systemd/system.conf.d/efi-arch.conf \
        /etc/systemd/system/init-efi-bootmenu.service
+    rpi_kernel
   fi
   # Enable serial console
   systemctl enable serial-getty@ttyS0
@@ -61,4 +62,47 @@ boot() {
 
 boot_cleanup() {
   rm -rf /usr/lib/dracut # Remove the leftovers we copied into the image
+}
+
+rpi_kernel() {
+  local kver kerneltmp rpi_suffix
+  kerneltmp=$(mktemp -d)
+  wget -qO>(tar -xzC "$kerneltmp" --strip-components=1) \
+    "https://github.com/raspberrypi/firmware/archive/5eaa42401e9625415934c97baae5a8d65933768c.tar.gz"
+  case "$VARIANT" in
+    rpi2|rpi3) rpi_suffix=7 ;;
+    rpi4) rpi_suffix=8 ;;
+    rpi5) rpi_suffix=_2712 ;;
+  esac
+  uname_path="$kerneltmp/extra/uname_string${rpi_suffix}"
+  kver=$(grep -o '^Linux version [^ ]\+' "$uname_path" | cut -d' ' -f3)
+  mkdir /usr/lib/modules
+  cp -r "$kerneltmp/modules/$kver" "/usr/lib/modules/$kver"
+  cp "$kerneltmp/boot/kernel${rpi_suffix}.img" "/boot/vmlinuz-$kver"
+  ln -s "boot/vmlinuz-$kver" /vmlinuz
+  ln -s "boot/vmlinuz-$kver" /vmlinuz.old
+
+  mkdir /boot/firmware
+  cp -r "$kerneltmp/boot/overlays" /boot/firmware/overlays
+  case "$VARIANT" in
+    rpi2)
+      cp "$kerneltmp/boot/start_x.elf" \
+          "$kerneltmp/boot"/bcm*-rpi-2* \
+          /boot/firmware
+      ;;
+    rpi3)
+      cp "$kerneltmp/boot/start_x.elf" \
+          "$kerneltmp/boot"/bcm2710-rpi-3* \
+          /boot/firmware
+      ;;
+    rpi4)
+      cp "$kerneltmp/boot/start4x.elf" \
+          "$kerneltmp/boot"/bcm2711* \
+          /boot/firmware
+      ;;
+    rpi5)
+      cp "$kerneltmp/boot"/bcm2712* /boot/firmware
+      ;;
+  esac
+  rm -rf "$kerneltmp"
 }
